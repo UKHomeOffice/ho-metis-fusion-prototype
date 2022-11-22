680b07e9f13e8a044888ce01dbc3c6048be94856
Enor Anidi-Ryz <hello@enoranidi.com>
2022-11-22 14:21:35 +0000

    New content added

--- app/routes.js
+++ app/routes.js
@@ -4,17 +4,13 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 router.get("/prototype-start", (req, res) => {
-    // req.session.data["my-tasks"] = "my tasks"
-    // req.session.data["track-requests"] = "track my requests"
-    // req.session.data["annual-leave"] = "annual leave"
-    // req.session.data["pay"] = "pay"
-    // req.session.data["team-absences"] = "team absences"
-    // req.session.data["team-performance-rewards"] = "team performance rewards"
-    // req.session.data["find-someone"] = "find someone"
-    // req.session.data["get-help"] = "get help"
-    // req.session.data["guidance"] = "guidance"
     req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "team-absences", "team-performance-rewards", "find-someone", "get-help", "guidance"]
     res.redirect("line-manager/index")
+})
+
+router.get("/prototype-start-general", (req, res) => {
+    req.session.data["personalise"] = ["my-tasks-2", "track-my-requests-2", "annual-leave-2", "pay-2", "expenses-2", "learning-2", "find-someone-2", "get-help-2", "guidance-2"]
+    res.redirect("general/index")
 })
 
 // router.use("/prototype-start", router)
