8d18a73becd9b7e9301f733e63b310447fa35c5b
Enor Anidi-Ryz <hello@enoranidi.com>
2022-11-29 13:51:36 +0000

    Updates to the prototype

--- app/routes.js
+++ app/routes.js
@@ -8,9 +8,19 @@ router.get("/prototype-start", (req, res) => {
     res.redirect("line-manager/index")
 })
 
+router.get("/prototype-start-notification", (req, res) => {
+    req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "team-absences", "team-performance-rewards", "find-someone", "get-help", "guidance"]
+    res.redirect("line-manager/index-notification")
+})
+
 router.get("/prototype-start-general", (req, res) => {
     req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "expenses", "learning", "find-someone", "get-help", "guidance"]
     res.redirect("general/index")
+})
+
+router.get("/prototype-start-general-notification", (req, res) => {
+    req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "team-absences", "team-performance-rewards", "find-someone", "get-help", "guidance"]
+    res.redirect("general/index-notification")
 })
 
 // router.use("/prototype-start", router)
