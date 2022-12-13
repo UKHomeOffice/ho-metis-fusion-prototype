b497296f68d89082c38f1b2660cee284e43ce4e5
cahil-ho <cahil.demenezes@digital.homeoffice.gov.uk>
2022-12-13 10:46:52 +0000

    added logic to hide and show code depending on user type

--- app/routes.js
+++ app/routes.js
@@ -5,21 +5,25 @@ const router = express.Router()
 
 router.get("/prototype-start", (req, res) => {
   req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "team-absences", "team-performance-rewards", "find-someone", "get-help", "guidance", "metis-alerts"]
+  req.session.data["user-type"] = "line-manager"
   res.redirect("line-manager/index")
 })
 
 router.get("/prototype-start-notification", (req, res) => {
   req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "team-absences", "team-performance-rewards", "find-someone", "get-help", "guidance", "metis-alerts"]
+  req.session.data["user-type"] = "line-manager"
   res.redirect("line-manager/index-notification")
 })
 
 router.get("/prototype-start-general", (req, res) => {
   req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "expenses", "learning", "find-someone", "get-help", "guidance", "metis-alerts"]
+  req.session.data["user-type"] = "general"
   res.redirect("general/index")
 })
 
 router.get("/prototype-start-general-notification", (req, res) => {
   req.session.data["personalise"] = ["my-tasks", "track-my-requests", "annual-leave", "pay", "expenses", "learning", "find-someone", "get-help", "guidance", "metis-alerts"]
+  req.session.data["user-type"] = "general"
   res.redirect("general/index-notification")
 })
 
